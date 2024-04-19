fb6b075241b245039a76c4991ae1333e6fae37c1
Greggles182 <gregory@echengineering.co.uk>
2024-04-19 18:12:33 +0100

    add hashing functiuons

---
+++ hasher.py
@@ -0,0 +1,18 @@
+# Hashing password using passlib
+from passlib.hash import sha256_crypt
+def hash(password):
+    # hash the password using sha256_crypt with 5000 rounds and a random salt
+    hashed = sha256_crypt.using(rounds=5000).hash(password)
+
+    # verify the hashed password against the plain password
+    if (sha256_crypt.verify(password, hashed)):
+        return hashed
+def verify(password, hash):
+    # verify the hashed password against the plain password
+    if (sha256_crypt.verify(password, hash)):
+        return True
+    else:
+        return False
+if __name__ == "__main__":
+    print(hash("password"))
+    print(verify("password", "$5$qG4UNBb2uM1NWhFE$/Nr0T8MSV.Wpg/ftCPTETI3GiL2kdL2IBvc5FB0a002"))
