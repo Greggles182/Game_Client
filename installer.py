0f3be6165580c4b5e6fa60ccd3f2b15791fa5333
Greggles182 <gregory@echengineering.co.uk>
2024-04-19 17:44:24 +0100

    fvfdtfjy

--- installer.py
+++ installer.py
@@ -29,16 +29,17 @@ try:
         os.system(clone_with_path)
         os.chdir(destination_path)
         try:
-            import pygame, requests
-        except Exception as e:
-            print("There was an issue checking whether pygame and requests are installed")
-            print("Error: " + e)
-            print("Installing both:")
+            import pygame, requests, passlib
+        except Exception:
+            print("There was an issue checking whether necesary are installed")
+            print("Installing all:")
             os.system("python -m pip install --upgrade pygame")
             os.system("python -m pip install --upgrade requests")
+            os.system("python -m pip install --upgrade passlib")
         from importlib.metadata import version
         v1 = version('pygame')
         v2 = version('requests')
+        v4 = version('passlib')
         if v1 >= "2.5.2":
             print("Pygame up to date")
         else:
@@ -49,14 +50,32 @@ try:
         else:
             print("Updating requests")
             os.system("python -m pip install --upgrade requests")
+        if v2 >= "1.7.4":
+            print("Hashing tool up to date")
+        else:
+            print("Updating passlib")
+            os.system("python -m pip install --upgrade passlib")
         if Aftercpy:
             print("You will need to replace the levels folder in the Game directory with levels-backup in the current directory, and replace the rlog.pkl file.")
         print("Install complete. Please run main.py in the Game directory to launch")
     if inst == "2" or inst == "3":
+        try:
+            from importlib.metadata import version
+            import flask
+            v1 = version('flask')
+            if v1 >= "3.0.3":
+                print("Flask up to date")
+            else:
+                print("Updating flask")
+                os.system("python -m pip install --upgrade flask")
+        except Exception:
+            print("There was an issue checking whether flask is installed")
+            print("Installing flask:")
+            os.system("python -m pip install --upgrade flask")
         destination_path = "Server/"
         if os.path.isdir(destination_path):
             shutil.rmtree(destination_path, ignore_errors=True)
-            input("Please ensure the server folder no longer exists, THEN press Enter")
+            input("Please ensure the 'Server' folder no longer exists, THEN press Enter")
         clone_command = "git clone https://github.com/Greggles182/Game_Server.git"
         clone_with_path = clone_command + " " + destination_path
         os.system(clone_with_path)
@@ -64,6 +83,7 @@ try:
         print("Install Success!")
     else:
         raise ValueError
-except Exception as e:
+except ModuleNotFoundError:
+    print("There was an Module error running the program.")
+except Exception:
     print("There was an issue running the program.")
-    print(e)
