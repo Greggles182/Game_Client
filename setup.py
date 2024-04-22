08285d318ba41ed79feb79bcb3c279a649f0d0d8
Greggles182 <gregory@echengineering.co.uk>
2024-04-22 19:48:14 +0100

    Freezer Build

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, Executable
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+opts = {'include_files' : ["img/", "img2/", "levels/", "music/"]}
+
+setup(
+    name = "Game_CLIENT",
+    version = "1.0",
+    description = "A game made by Gregory, Rory and Sam for Y7 Asessment",
+    options = {'build_exe': opts},
+    executables = [
+        Executable("main.py", base=base),
+        Executable("resetonline.py", base=base)])
