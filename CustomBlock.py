4866574774f356b860465cd630a45f1ef811857c
Greggles182 <gregory@echengineering.co.uk>
2024-05-02 19:56:38 +0100

    Something

--- CustomBlock.py
+++ CustomBlock.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import messagebox as box
 from tk1 import TKinput as input
 import subprocess, pygame, os, game, sys, threading, webclient, pickle, random, time
+import PIL.Image as Image
+import numpy as np
 def graphicdesign():
     pygame.font.init()
     import LevelInput
@@ -182,6 +184,199 @@ def graphicdesign():
         pygame.display.update()
 
         #Designs a single level.
+def leveldesign(cblocks):
+    pygame.font.init()
+    import LevelInput
+    clock = pygame.time.Clock()
+    FPS = 60
+
+    #game window
+    SCREEN_WIDTH = 640
+    SCREEN_HEIGHT = 640
+    LOWER_MARGIN = 100
+    SIDE_MARGIN = 320
+
+    screen = pygame.display.set_mode(
+        (SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT + LOWER_MARGIN))
+    pygame.display.set_caption("Level Editor")
+
+    #define game variables
+    ROWS = 20
+    MAX_COLS = 20
+    TILE_SIZE = SCREEN_HEIGHT // ROWS
+    TILE_TYPES = 9
+    current_tile = 0
+    scroll = 0
+
+    #load images
+    #pine1_img = pygame.image.load("img2/Background/pine1.png").convert_alpha()
+    #pine2_img = pygame.image.load("img2/Background/pine2.png").convert_alpha()
+    #mountain_img = pygame.image.load("img2/Background/mountain.png").convert_alpha()
+    sky_img = pygame.image.load(
+        "img/Background/background.png").convert_alpha()
+    #store tiles in a list
+    img_list = []
+    for x in range(TILE_TYPES):
+        img = pygame.image.load(f"img/tile/{x}.png").convert_alpha()
+        img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+        img_list.append(img)
+
+    save_img = pygame.image.load("img/play_btn.png").convert_alpha()
+    load_img = pygame.image.load("img/CLI_btn.png").convert_alpha()
+
+    #define colours
+    GREEN = (144, 201, 120)
+    WHITE = (255, 255, 255)
+    RED = (200, 25, 25)
+
+    #define font
+    font = pygame.font.SysFont("Futura", 30)
+
+    #create empty tile list
+    world_data = []
+    for row in range(ROWS):
+        r = [0] * MAX_COLS
+        world_data.append(r)
+
+    #create ground
+    for tile in range(0, MAX_COLS):
+        world_data[ROWS - 1][tile] = 2
+
+    class Button():
+
+        def __init__(self, x, y, image, scale):
+            width = image.get_width()
+            height = image.get_height()
+            self.image = pygame.transform.scale(
+                image, (int(width * scale), int(height * scale)))
+            self.rect = self.image.get_rect()
+            self.rect.topleft = (x, y)
+            self.clicked = False
+
+        def draw(self, surface):
+            action = False
+
+            #get mouse position
+            pos = pygame.mouse.get_pos()
+
+            #check mouseover and clicked conditions
+            if self.rect.collidepoint(pos):
+                if pygame.mouse.get_pressed(
+                )[0] == 1 and self.clicked == False:
+                    action = True
+                    self.clicked = True
+
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+            #draw button
+            surface.blit(self.image, (self.rect.x, self.rect.y))
+
+            return action
+
+    #function for outputting text onto the screen
+    # def draw_text(text, font, text_col, x, y):
+    # 	img = font.render(text, True, text_col)
+    # 	screen.blit(img, (x, y))
+
+    #create function for drawing background
+    def draw_bg():
+        screen.fill(GREEN)
+        width = sky_img.get_width()
+        for x in range(4):
+            screen.blit(sky_img, ((x * width) - scroll * 0.5, 0))
+            #screen.blit(mountain_img, ((x * width) - scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+            #screen.blit(pine1_img, ((x * width) - scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
+            #screen.blit(pine2_img, ((x * width) - scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
+
+    #draw grid
+    def draw_grid():
+        #vertical lines
+        for c in range(MAX_COLS + 1):
+            pygame.draw.line(screen, WHITE, (c * TILE_SIZE - scroll, 0),
+                             (c * TILE_SIZE - scroll, SCREEN_HEIGHT))
+        #horizontal lines
+        for c in range(ROWS + 1):
+            pygame.draw.line(screen, WHITE, (0, c * TILE_SIZE),
+                             (SCREEN_WIDTH, c * TILE_SIZE))
+
+    #function for drawing the world tiles
+    def draw_world():
+        for y, row in enumerate(world_data):
+            for x, tile in enumerate(row):
+                if tile >= 0:
+                    screen.blit(img_list[tile],
+                                (x * TILE_SIZE - scroll, y * TILE_SIZE))
+
+    #create buttons
+    save_button = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT + LOWER_MARGIN - 50,
+                         save_img, 1)
+    load_button = Button(SCREEN_WIDTH // 2 + 200,
+                         SCREEN_HEIGHT + LOWER_MARGIN - 50, load_img, 1)
+    #make a button list
+    button_list = []
+    button_col = 0
+    button_row = 0
+    for i in range(len(img_list)):
+        tile_button = Button(SCREEN_WIDTH + (75 * button_col) + 50,
+                             75 * button_row + 50, img_list[i], 1)
+        button_list.append(tile_button)
+        button_col += 1
+        if button_col == 3:
+            button_row += 1
+            button_col = 0
+
+    run = True
+    while run:
+
+        clock.tick(FPS)
+
+        draw_bg()
+        draw_grid()
+        draw_world()
+
+        #run levelbuilder
+        if save_button.draw(screen):
+            return world_data
+        
+        if load_button.draw(screen):
+            rsp = LevelInput.cli(world_data)
+            world_data = rsp
+        #draw tile panel and tiles
+        pygame.draw.rect(screen, GREEN,
+                         (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
+        
+        #choose a tile
+        button_count = 0
+        for button_count, i in enumerate(button_list):
+            if i.draw(screen):
+                current_tile = button_count
+
+        #highlight the selected tile
+        pygame.draw.rect(screen, RED, button_list[current_tile].rect, 3)
+
+        #add new tiles to the screen
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        x = (pos[0] + scroll) // TILE_SIZE
+        y = pos[1] // TILE_SIZE
+
+        #check that the coordinates are within the tile area
+        if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGHT:
+            #update tile value
+            if pygame.mouse.get_pressed()[0] == 1:
+                if world_data[y][x] != current_tile:
+                    world_data[y][x] = current_tile
+            if pygame.mouse.get_pressed()[2] == 1:
+                world_data[y][x] = -1
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+        #Designs a single level.
     
 def create():
     name = input("Welcome to the custom block builder. What should it be named?")
@@ -197,10 +392,49 @@ def create():
     maps = [[],[],[]]
     for i in range(num):
         input("Now we need to create a graphic.", False)
-        maps[i] = graphicdesign()
+        mapTMP = graphicdesign()
+        for b, n in enumerate(mapTMP):
+            if n == 0:
+                mapTMP[b] = (255, 255, 255)
+            elif n == 1:
+                mapTMP[b] = (255, 0, 0)
+            elif n == 2:
+                mapTMP[b] = (0, 255, 0)
+            elif n == 3:
+                mapTMP[b] = (0, 0, 255)
+            elif n == 4:
+                mapTMP[b] = (255, 255, 0)
+            elif n == 5:
+                mapTMP[b] = (0, 255, 255)
+            elif n == 6:
+                mapTMP[b] = (255, 0, 255)
+            elif n == 7:
+                mapTMP[b] = (0, 0, 0)
+            elif n == 8:
+                mapTMP[b] = (154, 154, 154)
+            elif n == 9:
+                mapTMP[b] = (104.6, 104.6, 104.6)
+        maps[i] = mapTMP
+        # Convert the pixels into an array using numpy
+        array = np.array(mapTMP, dtype=np.uint8)
+
+        # Create a grayscale image
+        grayscale_image = Image.fromarray(array, 'L')
+
+        # Create a new image with the same size as the grayscale image
+        colored_image = Image.new('RGB', grayscale_image.size)
+
+        # Apply the color palette to the new image
+        colored_image.paste(grayscale_image)
+
+        # Save the colored image
+        colored_image.save(f'./levels/img/TEMP{i}.png')
+
         input("Now that we have the graphic, we need to create code.\n See http://gregglesthegreat.pythonanywhere.com/help for assistance.", False)
         os.system(f"start notepad.exe ./levels/code/{name}{i}.py")
         input("Press Ctrl+S on the code to save, close the editor and then press Submit.", False)
+
+    
     if os.path.isfile(f"levels/{name}.pkl"):
         if box.askokcancel("File Exists!", "Continue?") == 1:
             pass
@@ -208,8 +442,9 @@ def create():
             create()
     else:
         pass
-    with open(f"levels/{name}.pkl", "wb"):
-        pickle.dump({"data":[], "leveldata":[], "code": open(f"levels/code/{name}.py", "rt").read(), "graphics":{"1":map}})
+    input("Now we need to draw the level.", False)
+    # with open(f"levels/{name}.pkl", "wb"):
+    #     pickle.dump({"data":[], "leveldata":[], "code": open(f"levels/code/{name}{i}.py", "rt").read(), "graphics":{"1":map}})
 
     
 
